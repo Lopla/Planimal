5e66588cd73be82e3e776dff8ccaed21364beb74
Pawel Galezowski <pgalezowski@gmail.com>
2022-06-16 20:18:06 +0200

    initial red ciricle

---
+++ Planimal/Ctx.cs
@@ -0,0 +1,41 @@
+using Pla.Lib;
+using SkiaSharp;
+
+namespace Planimal
+{
+    internal class Ctx : IContext, IControl, IPainter
+    {
+        public void Click(SKPoint argsLocation)
+        {
+            
+        }
+
+        public IControl GetControl()
+        {
+            return this;
+        }
+
+        public IPainter GetPainter()
+        {
+            return this;
+        }
+
+        public void Init(IEngine engine)
+        {
+
+            //engine.RequestTransparentWindow();
+        }
+
+        public void KeyDown(uint key)
+        {
+            
+        }
+
+        public void Paint(SKImageInfo info, SKSurface surface)
+        {
+            surface.Canvas.DrawCircle(100,100,50,new SKPaint(){
+                Color = SKColor.Parse("f00")
+            });
+        }
+    }
+}
